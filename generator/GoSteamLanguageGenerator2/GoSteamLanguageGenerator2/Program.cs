55821d51993d7742fd3218f79867f1805914fd8c
Jaxh <jaxhlee@qq.com>
2023-08-22 19:23:29 +0800

    fix for dota

---
+++ generator/GoSteamLanguageGenerator2/GoSteamLanguageGenerator2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoSteamLanguageGenerator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
